3f66b62d60c327f74a21054d1e7068afc6643596
Rajib Ahmed <rajib.ahmed@klarna.com>
2020-01-31 13:40:14 +0100

    useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useReducer } from "react";
 import "./App.css";
 
 const userApi = "https://randomuser.me/api/";
+
+const CHANGE_PAGE = "EVENT/CHANGE_PAGE";
+const initialState = { page: 1 };
+
+function changePageReducer(state, action) {
+  switch (action.type) {
+    case CHANGE_PAGE:
+      return { page: state.page + 1 };
+    default:
+      return state;
+  }
+}
 
 const UserData = ({ user, children }) => {
   return (
     <>
       <h3> {user.name.first}</h3>
       <h3> {user.email}</h3>
-      {children}
+      <img src={user.picture.large} alt={user.email} />
     </>
   );
 };
@@ -22,20 +34,29 @@ const User = ({ user }) => {
   );
 };
 
+const changePageAction = dispatch => () => dispatch({ type: CHANGE_PAGE });
+
 function App() {
+  const [state, dispatch] = useReducer(changePageReducer, initialState);
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    fetch(userApi)
+    let api = userApi + "?page=" + state.page;
+    fetch(api)
       .then(res => res.json())
       .then(data => {
         const u = data.results[0];
         setUser(u);
       })
-      .catch(err => console.log);
-  }, []);
+      .catch(console.log);
+  }, [state.page]);
 
-  return <div className="App">{user && <User user={user} />}</div>;
+  return (
+    <div className="App">
+      {user && <User user={user} />}
+      <button onClick={changePageAction(dispatch)}>Change Page</button>
+    </div>
+  );
 }
 
 export default App;
